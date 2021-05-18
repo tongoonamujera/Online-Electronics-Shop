2ea2262826b7144da346586c200a3dd53b395e7f
tongoonamujera <tongoonamujera@gmail.com>
2021-05-18 16:02:25 +0200

    added a route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :televisions
   resources :laptops
   resources :cell_phones
+  root: "cell_phones#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
